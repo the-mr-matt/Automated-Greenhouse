52042d5c01c2515292c966865dfd850f85fd940a
Matthew Inglis <matthew@winglett.co.nz>
2018-07-18 09:16:09 +1200

    Added LCD component test
    Found a library for LCDs using shift registers.
    
    https://playground.arduino.cc/Main/LiquidCrystal#Download

---
+++ Component Testing/LCD-Shift-Register/LCD-Shift-Register.ino
@@ -0,0 +1,20 @@
+//----LIBRARIES----
+#include <SPI.h>
+#include <LiquidCrystal.h>
+
+//----CONFIG----
+LiquidCrystal lcd(9);
+
+void setup()
+{
+  lcd.begin(16, 2);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World!");
+}
+
+void loop()
+{
+  lcd.setCursor(0, 1);
+  lcd.print(millis());
+}
